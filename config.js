56700cf22aa16992a43496888f3d52e02fea8e8b
lunagreen705 <vip1201788339241@gmail.com>
2025-05-30 15:00:08 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   TOKEN: "",
-  language: "en",
+  language: "tw",
   ownerID: ["1004206704994566164", ""], 
   mongodbUri : "",
   spotifyClientId : "",
@@ -10,8 +10,8 @@ module.exports = {
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
-  activityName: "YouTube Music", 
-  activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
+  activityName: "i don t want set the world on fire", 
+  activityType: "PLAYING",  // Available activity types : LISTENING , PLAYING
   SupportServer: "https://discord.gg/xQF9f9yUEM",
   embedTimeout: 5, 
   errorLog: "", 
