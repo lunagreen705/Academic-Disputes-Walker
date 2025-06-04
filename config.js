70fe965fe587ad10ae60d93e89baafd0ed8a2fdc
lunagreen705 <vip1201788339241@gmail.com>
2025-06-04 15:26:07 +0800

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,6 @@ module.exports = {
   embedColor: "#1db954",
   activityName: "i don t want set the world on fire", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
-  SupportServer: "https://discord.gg/xQF9f9yUEM",
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
