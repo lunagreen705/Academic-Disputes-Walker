4871c66c3341813bd50480d022730817feeadc73
lunagreen705 <vip1201788339241@gmail.com>
2025-06-04 13:51:18 +0800

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -6,7 +6,7 @@ const musicIcons = require('../UI/icons/musicicons.js');
 
 module.exports = {
   name: "help",
-  description: "Get information about the bot",
+  description: "取得機器人相關幫助",
   permissions: "0x0000000000000800",
   options: [],
   run: async (client, interaction, lang) => {
