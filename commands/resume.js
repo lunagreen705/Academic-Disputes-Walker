c43d3f50d5cdab886f678d417c80322ce2d9d43b
lunagreen705 <vip1201788339241@gmail.com>
2025-06-04 14:00:37 +0800

    Update resume.js

--- commands/resume.js
+++ commands/resume.js
@@ -53,7 +53,7 @@ async function resume(client, interaction, lang) {
 
 module.exports = {
     name: "resume",
-    description: "Resume the current song",
+    description: "繼續播放歌曲",
     permissions: "0x0000000000000800",
     options: [],
     run: resume
